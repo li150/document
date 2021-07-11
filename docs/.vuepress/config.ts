2032148d86fb0579bb79baa3436c762d9bd1cf09
lyc <1449770614@qq.com>
2021-07-11 15:03:08 +0800

    自动化部署4

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ const filePath = path.resolve('./docs');
 const fileArr = []
 //文件遍历方法
 function fileDisplay(filePath) {
-    //根据文件路径读取文件，返回文件列表 999
+    //根据文件路径读取文件，返回文件列表 888
     fs.readdir(filePath,function(err,files){
         if(err) {
             console.warn(err)
